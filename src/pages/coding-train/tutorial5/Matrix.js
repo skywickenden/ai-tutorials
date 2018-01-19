72555b9adcd2af2630a5a99bc25ccf727205d296
skywickenden <github@skywickenden.co.uk>
2018-01-19 10:32:57 +0000

    Matrix addition

--- src/pages/coding-train/tutorial5/Matrix.js
+++ src/pages/coding-train/tutorial5/Matrix.js
@@ -14,9 +14,34 @@ class Matrix {
 
   randomize() {
     for (let i = 0; i < this.rows; i++) {
-      this.matrix[i] = [];
       for (let j = 0; j < this.cols; j++) {
         this.matrix[i][j] = Math.floor(Math.random() * 10);
+      }
+    }
+  }
+
+  add(addend) {
+    if (this.cols != addend.cols) {
+      console.error(
+        'When adding matrixes, the columns must be of equal length. Currently',
+        this.cols,
+        addend.cols
+      );
+      return;
+    }
+
+    if (this.rows != addend.rows) {
+      console.error(
+        'When adding matrixes, the rows must be of equal length. Currently',
+        this.rows,
+        addend.rows
+      );
+      return;
+    }
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.matrix[i][j] += addend.matrix[i][j];
       }
     }
   }
--- src/pages/coding-train/tutorial5/main.js
+++ src/pages/coding-train/tutorial5/main.js
@@ -2,9 +2,14 @@ var brain;
 function setup() {
   brain = new NeuralNetwork(3, 3, 1);
   const testMatrix = new Matrix(3, 2);
-  console.table(testMatrix.matrix);
+  const testMatrix2 = new Matrix(3, 2);
   testMatrix.randomize();
+  testMatrix2.randomize();
   console.table(testMatrix.matrix);
+  console.table(testMatrix2.matrix);
+  testMatrix.add(testMatrix2);
+  console.table(testMatrix.matrix);
+
 }
 
 
